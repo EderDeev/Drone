4ea99f73cc8a37478bd22d344e97fe14baabaa90
Ederson Moreira <114446581+EderDeev@users.noreply.github.com>
2024-12-10 15:24:21 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-viaduto_independncia_09-09-2024",
+      "name": "VIADUTO_INDEPENDÊNCIA_09-09-2024",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.024035525137762193,
+        "pitch": 1.5707963267948966,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4733288141668268,
+          "pitch": -0.2414691954821251,
+          "rotation": 0,
+          "target": "1-viaduto_ala_viaria_09-09-2024"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-viaduto_ala_viaria_09-09-2024",
+      "name": "VIADUTO_ALÇA_VIARIA_09-09-2024",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.023152326812169832,
+        "pitch": 1.5707963267948966,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6520200403051462,
+          "pitch": -0.39351079735685524,
+          "rotation": 0,
+          "target": "0-viaduto_independncia_09-09-2024"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
